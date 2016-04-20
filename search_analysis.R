1da62381b0781db52dd827182079a9cc96cb523f
Alexander Asplund <alexander.asplund@visma.com>
2016-04-20 22:35:10 +0200

    Add wordclouds of search terms

--- search_analysis.R
+++ search_analysis.R
@@ -1,4 +1,5 @@
 library(tm)
+library(wordcloud)
 
 source('data_processing/parse_json_data.R')
 
@@ -7,8 +8,14 @@ searches <- parseJsonSearches('data/searches')
 searches$query <- tolower(searches$query)
 searchesWithoutNavigation <- searches[! grepl('->', searches$query), ]
 
+queryCounts <- as.data.frame(table(searchesWithoutNavigation$query))
+
 words <- paste(searchesWithoutNavigation$query, collapse = ' ')
 
 wordCounts <- as.data.frame(table(strsplit(words, split = " ")))
 
 wordCounts <- wordCounts[! wordCounts$Var1 %in% stopwords(), ]
+
+searchcloud_min_100 <- wordcloud(words, min.freq = 100)
+searchcloud_min_50 <- wordcloud(words, min.freq=50)
+
