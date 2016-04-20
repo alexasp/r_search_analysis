eaf257de8767e9b667fd67a4d1b52bd89f569e7c
Alexander Asplund <alexander.asplund@visma.com>
2016-04-20 16:32:51 +0200

    Initial commit.

---
+++ search_analysis.R
@@ -0,0 +1,14 @@
+library(tm)
+
+source('parse_json_data.R')
+
+
+searches <- parseJsonSearches('data/searches')
+searches$query <- tolower(searches$query)
+searchesWithoutNavigation <- searches[! grepl('->', searches$query), ]
+
+words <- paste(searchesWithoutNavigation$query, collapse = ' ')
+
+wordCounts <- as.data.frame(table(strsplit(words, split = " ")))
+
+wordCounts <- wordCounts[! wordCounts$Var1 %in% stopwords(), ]
